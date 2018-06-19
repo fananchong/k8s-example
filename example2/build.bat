6186ba4966746696ad264ea79663ebc7d1812e0b
fananchong <fananchong@126.com>
2018-06-19 12:01:02 +0800

    example2

--- example2/build.bat
+++ example2/build.bat
@@ -1,6 +1,5 @@
 set GOPATH=%~dp0
 set GOBIN=%~dp0\bin
 
-go install service_a.go
-go install service_b.go
-go install client.go
+go install service_a2.go
+go install service_b2.go
--- example2/build.sh
+++ example2/build.sh
@@ -6,8 +6,8 @@ if [ ! -x "bin" ]; then
     mkdir "bin"
 fi
 
-docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example2 golang go install ./service_a.go
-docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example2 golang go install ./service_b.go
+docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example2 golang go install ./service_a2.go
+docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example2 golang go install ./service_b2.go
 
 docker build -t k8s-example2 .
 
