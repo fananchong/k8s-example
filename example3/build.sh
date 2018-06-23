45f507dbd64c412978eb105a7193944862da4fb2
fananchong <fananchong@126.com>
2018-06-23 13:22:03 +0800

    example3

--- example3/build.sh
+++ example3/build.sh
@@ -6,17 +6,17 @@ if [ ! -x "bin" ]; then
     mkdir "bin"
 fi
 
-docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example2 golang go install ./service_a.go ./k8s_api.go
-docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example2 golang go install ./service_b.go ./k8s_api.go
+docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example3 golang go install ./service_a.go ./k8s_api.go
+docker run -it --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD"/../:/go/src/ -w /go/src/example3 golang go install ./service_b.go ./k8s_api.go
 
-docker build -t k8s-example2 .
+docker build -t k8s-example3 .
 
-docker tag k8s-example2:latest fananchong/k8s-example2:latest
+docker tag k8s-example3:latest fananchong/k8s-example3:latest
 
 
-# run for test
+set +ex
 
-docker push fananchong/k8s-example2:latest
+docker push fananchong/k8s-example3:latest
 
 kubectl apply -f ./namespace.yaml
 kubectl apply -f ./service_account.yaml
