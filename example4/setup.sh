f05c1d81dd1dd834ddef910d43ac8c19317665fd
root <root@localhost.tech>
2018-06-26 06:01:16 -0400

    example4

--- example4/setup.sh
+++ example4/setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-
 helm del --purge my-redis
 kubectl delete pvc --all -n my-redis
 kubectl delete -f pv.yaml
 kubectl create -f pv.yaml
 mkdir -p /tmp/data/local-redis-pv-1
-mkdir -p /tmp/data/local-redis-pv-2
-mkdir -p /tmp/data/local-redis-pv-3
-mkdir -p /tmp/data/local-redis-pv-4
-chmod a+rw /tmp/data/*
+chmod a+r+w /tmp/data/local-redis-pv-1
 helm install --name=my-redis --namespace=my-redis -f values-production.yaml stable/redis
